193d5c6b1ba782fe6e166b53318f6ca82528ccaa
kaalam <kaalam@dybot.com>
2018-03-19 19:52:28 +0100

    Applies a version number change removing the leading zero as required by cran

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 rm -rf rjazz.Rcheck
-rm rjazz_0.1.07.tar.gz
+rm rjazz_0.1.7.tar.gz
 
 R CMD REMOVE rjazz
 
 R CMD build rjazz
-R CMD check rjazz_0.1.07.tar.gz
+R CMD check rjazz_0.1.7.tar.gz
 
-R CMD INSTALL rjazz_0.1.07.tar.gz
+R CMD INSTALL rjazz_0.1.7.tar.gz
 
 rm rjazz-manual.pdf
 cp rjazz.Rcheck/rjazz-manual.pdf .
