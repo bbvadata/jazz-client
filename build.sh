424b884cd01683bdb44a3c1a8c144ab570e4cca2
kaalam <kaalam@dybot.com>
2018-03-19 21:19:30 +0100

    Makes build work even when the files were removed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf rjazz.Rcheck
-rm rjazz_0.1.7.tar.gz
+rm -f rjazz_0.1.7.tar.gz
 
 R CMD REMOVE rjazz
 
@@ -10,6 +10,6 @@ R CMD check rjazz_0.1.7.tar.gz
 
 R CMD INSTALL rjazz_0.1.7.tar.gz
 
-rm rjazz-manual.pdf
+rm -f rjazz-manual.pdf
 cp rjazz.Rcheck/rjazz-manual.pdf .
 rm -rf rjazz.Rcheck
